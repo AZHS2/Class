a0ef2c7ca1c78265830d666f36c4e4cd8964e84d
AZHS2 <99810919+AZHS2@users.noreply.github.com>
2022-02-16 19:06:42 +0600

    Update 2.py

--- JSON/2.py
+++ JSON/2.py
@@ -1,20 +1,22 @@
 import json
 from sys import stdin
 
-
 data = [i.rstrip() for i in stdin]
 data = [i.split(' = ')[1] for i in data]
 
-
 ans = {"complex": []}
 for i in data:
+    k = 0
     if len(i.split(' + ')) < 2:
         t = i.split(' - ')
+        k = 1
     else:
         t = i.split(' + ')
-    t[1] = t[1].split(' * ')[0]
+    if k == 1:
+        t[1] = "-" + t[1].split(' * ')[0]
+    else:
+        t[1] = t[1].split(' * ')[0]
     ans['complex'].append({"Re": float(t[0]), "Im": float(t[1])})
-
 
 with open('output.json', 'w', encoding='utf8') as out:
     json.dump(ans, out, indent=2)
